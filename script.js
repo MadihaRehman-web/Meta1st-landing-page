c1ca5539200db3de05c642a2056fc551771aebcf
Madiha Rehman <madiharehman654@gmail.com>
2025-10-19 03:30:11 +0500

    Implement dark/light mode toggle and minor maintenance improvement

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+const body = document.querySelector("body");
 const navBars = document.querySelector(".fa-bars");
 const navList = document.querySelector(".nav-list");
+const themeSwitch = document.querySelector(".switch");
 const logIn = document.querySelector(".log-in");
-console.log(navList);
+
+themeSwitch.addEventListener("change", () => {
+  console.log(themeSwitch);
+  body.classList.toggle("light-mode");
+  body.classList.toggle("dark-mode");
+});
 
 navBars.addEventListener("click", () => {
   navList.classList.toggle("hamburger");
